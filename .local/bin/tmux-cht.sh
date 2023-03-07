961286024b7c6fd4ea53ba3f55d63addc68f286d
erikroed <post@erikroed.no>
2023-03-07 23:51:47 +0100

    Restructure custom scripts folder and add bash_aliases

---
+++ .local/bin/tmux-cht.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+languages=$(echo "ansible terraform rust kotlin java python javascript typescript vue" | tr " " "\n")
+core_utils=$(echo "find xargs sed awk" | tr " " "\n")
+selected=$(echo -e "$languages\n$core_utils" | fzf)
+
+read -p "Query: " query
+
+if echo "$languages" | grep -qs $selected; then
+    tmux split-window -p 22 -h bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | less"
+else
+    tmux split-window -p 22 -h bash -c "curl cht.sh/$selected~$query | less
+fi

d9bd78a5d9fd1a34d0b674b07639975fb64a32f1
Erik Røed <post@erikroed.no>
2023-03-12 18:49:55 +0100

    Fix start of tmux from terminal with tmux-sessionizer.sh

--- bash/.local/bin/tmux-sessionizer.sh
+++ bash/.local/bin/tmux-sessionizer.sh
@@ -4,7 +4,7 @@ selected=$(find ~ ~/projects -mindepth 1 -maxdepth 1 -type d | fzf)
 selected_name=$(basename "$selected" | tr . _)
 
 if ! tmux has-session -t "$selected_name" 2> /dev/null; then
-    tmux new-session -s "$selected_name" -c "$selected" -d 
+    tmux attach -t "$selected_name" -c "$selected"
 fi
 
-tmux switch-client -t "$selected_name"
+tmux new-session -t "$selected_name"

514d88201da0144f0c88a24820d227af6554f122
Erik Røed <post@erikroed.no>
2023-09-20 00:14:41 +0200

    Replace packer with lazy.nvim

--- nvim/.config/nvim/lua/roed/init.lua
+++ nvim/.config/nvim/lua/roed/init.lua
@@ -1,12 +1,2 @@
 require("roed.set")
 require("roed.remap")
-
---[[
-call plug#begin()
-Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
-Plug 'junegunn/fzf.vim'
-call plug#end()
---]]
-
--- colorscheme desert
-

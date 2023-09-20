7c55916950a15b61134bf7b170c786b1962b3f1e
Erik Røed <post@erikroed.no>
2023-09-21 01:37:25 +0200

    Add treesitter, colors and harpoon

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -3,5 +3,12 @@ return {
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   { "rose-pine/neovim", name = "rose-pine" },
-  { 'nvim-telescope/telescope.nvim', tag = '0.1.3', dependencies = { 'nvim-lua/plenary.nvim' } }
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.3', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 
+      'nvim-treesitter/nvim-treesitter', build = ":TSUpdate", config = function()
+          require("nvim-treesitter.install").update({ with_sync = true })
+      end
+  },
+  { 'nvim-treesitter/playground' },
+  { 'theprimeagen/harpoon' },
 }
